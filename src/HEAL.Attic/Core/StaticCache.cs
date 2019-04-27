57191a5d6847285ceea9ce2321996c84d22cf655
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2019-04-27 13:23:57 +0200

    #8 Register more types from System.Collections and System.Collections.Generic

--- src/HEAL.Attic/Core/StaticCache.cs
+++ src/HEAL.Attic/Core/StaticCache.cs
@@ -69,17 +69,56 @@ namespace HEAL.Attic {
       RegisterType(new Guid("F0280B55-25E8-4981-B309-D675D081402A"), typeof(string));
 
       RegisterType(new Guid("9CF55419-439B-4A90-B2ED-8C7F7768EB61"), typeof(Array));
+      // interfaces in System.Collections
+      RegisterType(new Guid("3ce2470b-ffe4-45a3-b7a7-7722fa3060ac"), typeof(ICollection));
+      RegisterType(new Guid("cc245506-0711-4de0-8afe-f777152602af"), typeof(IComparer));
+      RegisterType(new Guid("028f6c38-da77-4308-9688-97c3bab4557e"), typeof(IDictionary));
       RegisterType(new Guid("B7313EE2-045F-41C9-AEC9-7B1A8CDBF90F"), typeof(IEnumerable));
-      RegisterType(new Guid("D5265599-7380-4A28-AB0D-9F336EF290C5"), typeof(IEnumerable<>));
+      RegisterType(new Guid("075827F4-07D9-4E50-9D00-C9FF3E7DCF9A"), typeof(IEqualityComparer));
       RegisterType(new Guid("E451A99B-0CFD-48E6-9BEF-0AE9D1E73628"), typeof(IList));
-      RegisterType(new Guid("DB2A785C-5803-42C7-A01F-E1FD845A83A1"), typeof(List<>));
-      RegisterType(new Guid("EB98A564-BEDC-458A-9E9B-4BF3CDAFB9BE"), typeof(Stack<>));
-      RegisterType(new Guid("D5E3118B-957A-43B5-A740-1BBAA0EAB666"), typeof(Stack));
+      RegisterType(new Guid("86e6372a-6b9f-43ab-b122-b7b00c216a44"), typeof(IStructuralComparable));
+      RegisterType(new Guid("fcc5e1a3-580b-4ffb-8734-fe6e6818ec32"), typeof(IStructuralEquatable));
+      // classes in System.Collections
       RegisterType(new Guid("8CBC5F9F-5671-4805-A690-DD034AEB7932"), typeof(ArrayList));
+      RegisterType(new Guid("1584c5be-1a51-423f-b7c6-d82e40cba012"), typeof(BitArray));
+      RegisterType(new Guid("fce7f2b0-7ed5-416a-b82f-ddb80da4ef42"), typeof(CollectionBase));
+      RegisterType(new Guid("ceb0ff31-724d-4d23-8bc1-fc35c714b6a0"), typeof(DictionaryBase));
+      RegisterType(new Guid("d509c307-f819-482f-8f56-71b97b487ced"), typeof(Hashtable));
       RegisterType(new Guid("80CAB5BF-06C2-4DB2-9CF0-EE6E93D40B30"), typeof(Queue));
-      RegisterType(new Guid("C528E123-F3DD-4711-83E2-A26E2A9A85E9"), typeof(Queue<>));
-      RegisterType(new Guid("7E2F3277-7216-4295-A498-9ACA43527D5B"), typeof(HashSet<>));
+      RegisterType(new Guid("163d715f-e3af-46db-8e07-d0548491b53d"), typeof(ReadOnlyCollectionBase));
+      RegisterType(new Guid("c92dd283-33e4-4576-a76a-a85620d2b44d"), typeof(SortedList));
+      RegisterType(new Guid("D5E3118B-957A-43B5-A740-1BBAA0EAB666"), typeof(Stack));
+      RegisterType(new Guid("8bf41fbf-bbe9-4974-8940-92a44031cc0f"), typeof(DictionaryEntry)); // struct
+      RegisterType(new Guid("65D2DE74-8BDF-4C74-9005-81A2C3991DC5"), Type.GetType("System.Collections.CompatibleComparer"));
+
+      // interfaces in System.Collections.Generic
+      RegisterType(new Guid("4b39c286-011d-47d5-9bbd-48f8ad49238d"), typeof(ICollection<>));
+      RegisterType(new Guid("e59ec88f-f615-4183-b439-20547ab68cb0"), typeof(IDictionary<,>));
+      RegisterType(new Guid("D5265599-7380-4A28-AB0D-9F336EF290C5"), typeof(IEnumerable<>));
+      RegisterType(new Guid("DAF22757-7FCC-49AC-B148-F3DD7E9E0A3B"), typeof(IEqualityComparer<>));
+      RegisterType(new Guid("bf40c8d9-e852-4f89-9b55-a6e615ca3561"), typeof(IList<>));
+      RegisterType(new Guid("27bad829-2f4d-42f4-bc10-a16831c37207"), typeof(IReadOnlyCollection<>));
+      RegisterType(new Guid("8ed582db-417f-43c0-b2f8-9d72cd7d5d55"), typeof(IReadOnlyDictionary<,>));
+      RegisterType(new Guid("52a285e5-c65f-49d3-87bf-fe995418c723"), typeof(IReadOnlyList<>));
+      RegisterType(new Guid("456cb7ea-5049-4535-a882-4a73bc9ef8cd"), typeof(ISet<>));
+      // classes in System.Collections.Generic
+      RegisterType(new Guid("5a254272-da2d-453a-8614-52e57911238b"), typeof(Comparer<>));
       RegisterType(new Guid("E92C35AD-32B1-4F37-B8D2-BE2F5FEB465B"), typeof(Dictionary<,>));
+      RegisterType(new Guid("C8BF5C03-1614-46AC-8730-2F231CAD4232"), typeof(EqualityComparer<>));
+      RegisterType(new Guid("7E2F3277-7216-4295-A498-9ACA43527D5B"), typeof(HashSet<>));
+      RegisterType(new Guid("DB2A785C-5803-42C7-A01F-E1FD845A83A1"), typeof(List<>));
+      RegisterType(new Guid("C528E123-F3DD-4711-83E2-A26E2A9A85E9"), typeof(Queue<>));
+      RegisterType(new Guid("cabd642e-fbef-4162-a413-ab7105975d09"), typeof(SortedDictionary<,>));
+      RegisterType(new Guid("744d8349-f2c8-45cf-81cd-6ec5fac9ba55"), typeof(SortedList<,>));
+      RegisterType(new Guid("f09956c0-17b4-47f4-8d9c-071cac9622e8"), typeof(SortedSet<>));
+      RegisterType(new Guid("EB98A564-BEDC-458A-9E9B-4BF3CDAFB9BE"), typeof(Stack<>));
+      RegisterType(new Guid("FB247FE0-9203-4196-B4D3-71AB18CD12C5"), Type.GetType("System.Collections.Generic.GenericEqualityComparer`1"));
+      RegisterType(new Guid("06443E04-FB1D-41E1-BAE2-1AB3728BDCC9"), Type.GetType("System.Collections.Generic.NullableEqualityComparer`1"));
+      RegisterType(new Guid("D9029E74-C511-4D28-B8C5-D0E299EC45A5"), Type.GetType("System.Collections.Generic.ObjectEqualityComparer`1"));
+      RegisterType(new Guid("C6D867E2-3CD2-4C59-8BA6-F18F00DDB997"), Type.GetType("System.Collections.Generic.ByteEqualityComparer"));
+      RegisterType(new Guid("02F8D180-BABE-4073-82E0-7FE77814F53D"), Type.GetType("System.Collections.Generic.EnumEqualityComparer`1"));
+      RegisterType(new Guid("D3D279FE-169E-46B6-93AE-589BB4F24A4D"), Type.GetType("System.Collections.Generic.LongEnumEqualityComparer`1"));
+
       RegisterType(new Guid("18504ADF-2332-4497-8CA0-B2F00CA2EBA2"), typeof(Type));
 
       RegisterType(new Guid("5EB35840-875C-4649-ACE1-8D16B86B0122"), Type.GetType("System.RuntimeType"));
@@ -87,16 +126,6 @@ namespace HEAL.Attic {
       RegisterType(new Guid("C7FDE8F0-EF3F-4A4D-8D87-5559C32A8B90"), Type.GetType("System.CultureAwareComparer"));
       RegisterType(new Guid("6A80741F-066D-460B-B333-4F766A4D4AEF"), Type.GetType("System.OrdinalComparer"));
       RegisterType(new Guid("94BB9853-8D25-4DD8-A915-FEF2EFF538BC"), Type.GetType("System.Resources.FastResourceComparer"));
-      RegisterType(new Guid("65D2DE74-8BDF-4C74-9005-81A2C3991DC5"), Type.GetType("System.Collections.CompatibleComparer"));
-      RegisterType(new Guid("075827F4-07D9-4E50-9D00-C9FF3E7DCF9A"), Type.GetType("System.Collections.IEqualityComparer"));
-      RegisterType(new Guid("C8BF5C03-1614-46AC-8730-2F231CAD4232"), Type.GetType("System.Collections.Generic.EqualityComparer`1"));
-      RegisterType(new Guid("FB247FE0-9203-4196-B4D3-71AB18CD12C5"), Type.GetType("System.Collections.Generic.GenericEqualityComparer`1"));
-      RegisterType(new Guid("06443E04-FB1D-41E1-BAE2-1AB3728BDCC9"), Type.GetType("System.Collections.Generic.NullableEqualityComparer`1"));
-      RegisterType(new Guid("D9029E74-C511-4D28-B8C5-D0E299EC45A5"), Type.GetType("System.Collections.Generic.ObjectEqualityComparer`1"));
-      RegisterType(new Guid("C6D867E2-3CD2-4C59-8BA6-F18F00DDB997"), Type.GetType("System.Collections.Generic.ByteEqualityComparer"));
-      RegisterType(new Guid("02F8D180-BABE-4073-82E0-7FE77814F53D"), Type.GetType("System.Collections.Generic.EnumEqualityComparer`1"));
-      RegisterType(new Guid("D3D279FE-169E-46B6-93AE-589BB4F24A4D"), Type.GetType("System.Collections.Generic.LongEnumEqualityComparer`1"));
-      RegisterType(new Guid("DAF22757-7FCC-49AC-B148-F3DD7E9E0A3B"), Type.GetType("System.Collections.Generic.IEqualityComparer`1"));
 
       // types form System.Drawing.Common
       RegisterType(new Guid("4151C39E-93BD-4B33-A0A9-84581C66235A"), typeof(System.Drawing.Bitmap));
